e03be2e5f6731e5321e3411315aff7bce6e31bae
rustan <rustanrr@mail.ru>
2018-02-25 02:54:05 +0400

    Registration

--- Books/urls.py
+++ Books/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Books.views import general
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', include('registration.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
     path('', general),
 ]
--- Books/views.py
+++ Books/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import auth
 
 
 def general(request):
-    return render(request, 'main.html')
+    args = {'user': auth.get_user(request).username}
+    return render(request, 'main.html', args)
