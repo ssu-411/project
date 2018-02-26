28e4b73cf967d921679e056865b60fe499334d8f
rustan <rustanrr@mail.ru>
2018-02-26 17:39:49 +0400

    refactor structure

--- Bookshelf/Books/urls.py
+++ Bookshelf/Books/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.urls import path, include
-from Books.views import general
+from Bookshelf.Books.views import general
 
 
 urlpatterns = [
