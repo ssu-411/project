c5ef5f01f97d03c2904a4cb2956281f998e7ea3b
rustan <rustanrr@mail.ru>
2018-03-01 13:20:43 +0400

    add missing part

--- bookshelf/book/models.py
+++ bookshelf/book/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    class Meta:
+        db_table = 'Books'
+
+    title = models.CharField(max_length=50)
+    author = models.ManyToManyField('Author')
+    rating = models.FloatField()
+    date = models.DateField()
+    publisher = models.ForeignKey('Publisher', on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField('Genre')
+
+    def __str__(self):
+        return self.title
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
