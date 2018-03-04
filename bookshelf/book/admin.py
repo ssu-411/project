0360bfc4ccf863b9527f61321a8cdbbf0189f2f4
rustan <rustanrr@mail.ru>
2018-03-04 16:46:50 +0400

    merge conflict fix

--- bookshelf/book/admin.py
+++ bookshelf/book/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
 from .models import Publisher, Book, Author, Genre
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(Publisher)
 admin.site.register(Author)
 admin.site.register(Genre)
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
