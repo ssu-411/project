d71102672a3b3dfcdf1a248b1ca165fa5669d775
rustan <rustanrr@mail.ru>
2018-03-09 12:33:15 +0400

    Change dirname

--- bookshelf/Books/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib import auth
-
-
-def general(request):
-    #args = {'user': auth.get_user(request).username}
-    return render(request, 'main.html', )#args)
