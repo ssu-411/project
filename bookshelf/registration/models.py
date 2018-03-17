af5e62514a02bdd76064728fcf4f31f8a22f8bb9
Victor Davydov <victor56152@gmail.com>
2018-03-17 04:19:58 +0400

    Revert "Added cmd download data with description in README"
    This reverts commit 123e93823ac9b43322300f5fbc74ec5493d1b966.

--- bookshelf/registration/models.py
+++ bookshelf/registration/models.py
@@ -11,19 +11,11 @@ genders = (
 
 class MyUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    to_read = models.ManyToManyField('book.Book', blank=True)
+    already_read = models.ManyToManyField('book.Book', blank=True)
     gender = models.CharField(max_length=10, choices=genders)
     age = models.PositiveSmallIntegerField()
     favorite_genres = models.ManyToManyField('book.Genre', blank=True)
     favorite_author = models.ManyToManyField('book.Author', blank=True)
-    rated_books = models.ManyToManyField('BookRating', blank=True)
 
     def __str__(self):
         return self.user.username
-
-class BookRating(models.Model):
-    rating = models.PositiveSmallIntegerField()
-    rtd_book = models.ForeignKey('book.Book', on_delete=models.PROTECT, null=False)
-
-    def __str__(self):
-        return "{:20s} -> {:d}".format(self.rtd_book.title, self.rating)
