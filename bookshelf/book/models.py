9e233dfe9107240d22cca5cb61de39cd6d105d01
Victurus <victor56152@gmail.com>
2018-03-19 17:21:10 +0400

    Adding work with large images. (#48) (#49)
    * Image fields,source data and settings update added
    
    * Added cmd download data with description in README
    
    To registration.MyUser model added filed rated_books.
    In registration was made a new model RatedBooks.
    It was made in order to give the user a way of marking books to be
    read in some future.
    
    Migrations added.
    README was enlarged by description of how to put the data from .csv
    files to database.
    
    * Timezone changed and now the life became easier :)
    
    * Provide small, middle and big images serving.
    
    README updated - added information about big images
    Changed Books model and added corresponding migrations.
    Added new layout for Books admin-site-page.
    The MEDIA files serving provided.
    downloaddata command changed in order to load big images.

--- bookshelf/book/models.py
+++ bookshelf/book/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import mark_safe
+from django.core.files.storage import default_storage
 
 # Create your models here.
 
@@ -10,9 +12,36 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.ManyToManyField('Author')
     rating = models.FloatField()
-    date = models.PositiveIntegerField(null=True)
-    publisher = models.ForeignKey('Publisher', on_delete=models.SET_NULL, null=True)
-    genre = models.ManyToManyField('Genre')
+    date = models.PositiveIntegerField(null=True, blank=True)
+    publisher = models.ForeignKey('Publisher', on_delete=models.SET_NULL, null=True, blank=True)
+    genre = models.ManyToManyField('Genre', blank=True)
+    smallImage = models.ImageField(upload_to='books', default='books/None/no-imgs.jpg', blank=True)
+    middleImage = models.ImageField(upload_to='books', default='books/None/no-imgm.jpg', blank=True)
+    bigImage = models.ImageField(upload_to='books', default='books/None/no-imgl.jpg', blank=True)
+
+    def smallImage_tag(self):
+        if default_storage.exists(self.smallImage):
+            return mark_safe('<img src="{:s}" />'.format(self.smallImage.url))
+        else:
+            return mark_safe('<img src="{:s}" />'.format("/media/books/None/no-imgs.jpg"))
+
+    smallImage_tag.short_description = "Small Image"
+
+    def middleImage_tag(self):
+        if default_storage.exists(self.middleImage):
+            return mark_safe('<img src="{:s}" />'.format(self.middleImage.url))
+        else:
+            return mark_safe('<img src="{:s}" />'.format("/media/books/None/no-imgm.jpg"))
+
+    middleImage_tag.short_description = "Middle Image"
+
+    def bigImage_tag(self):
+        if default_storage.exists(self.bigImage):
+            return mark_safe('<img src="{:s}" />'.format(self.bigImage.url))
+        else:
+            return mark_safe('<img src="{:s}" />'.format("/media/books/None/no-imgl.jpg"))
+
+    bigImage_tag.short_description = "Big Image"
 
     def __str__(self):
         return self.title
--- bookshelf/bookshelf/urls.py
+++ bookshelf/bookshelf/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from book.views import BooksView, BookView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +29,4 @@ urlpatterns = [
     path('books', BooksView.as_view()),
     path('books/p<page>', BooksView.as_view()),
     path('book/<id>', BookView.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
