9a878942ded3f2c1dc75bf09edfefe58a4de5bbd
rustan <rustanrr@mail.ru>
2018-03-25 21:20:46 +0400

    Modify login system

--- bookshelf/registration/views.py
+++ bookshelf/registration/views.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import PasswordChangeView, login
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.forms import UserCreationForm
 
 from registration.models import MyUser
-from .forms import MyUserRegisterForm, UserRegisterForm, PasswordChangeCustomForm
+from .forms import MyUserRegisterForm, UserRegisterForm, PasswordChangeCustomForm, AuthForm
 from django.views.decorators.csrf import csrf_protect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 
 
 # Create your views here.
@@ -42,6 +43,7 @@ class Register(CreateView):
 
             my_user.user = user
             my_user.save()
+            login(self.request, user)
             return redirect('/')
         else:
             return render(request, 'signup.html', {
@@ -70,3 +72,35 @@ class CustomPasswordChangeView(PasswordChangeView):
         self.extra_context['changed'] = True
         user_id = self.request.get_raw_uri().split('user/')[1].split('/')[0]
         return '/user/{}'.format(user_id)
+
+
+class Login(FormView):
+    form_class = AuthForm
+
+    @method_decorator(csrf_protect)
+    def get(self, request, *args, **kwargs):
+        data = {
+            'form': self.form_class(initial={'prev_page': '/'}),
+        }
+        return render(request, self.template_name, data)
+
+    @method_decorator(csrf_protect)
+    def post(self, request, *args, **kwargs):
+        prev_page = request.POST.get('prev_page')
+        if not prev_page:
+            prev_page = request.META.get('HTTP_REFERER', '/')
+        data = dict(request.POST.items())
+        data['prev_page'] = prev_page
+        form = self.form_class(data)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect(prev_page)
+            else:
+                form.add_error(None, "Неправильный логин и/или пароль")
+        return render(request, 'login.html', {
+            'form': form
+        })
