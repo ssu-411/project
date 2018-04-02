679f25a0474e710abf275310f0bbd073c6ded4b0
Rafikov Rustan <rustanrr@mail.ru>
2018-04-02 17:56:27 +0400

    Add auth tests (#73)
    * Add auth tests
    
    * Add tests to travis
    
    * Add create another travis job
    
    * Travis matrix
    
    * Add if-else to travis

--- bookshelf/registration/tests.py
+++ bookshelf/registration/tests.py
@@ -1,6 +1,42 @@
-from django.contrib.auth.models import User
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from registration.models import MyUser, genders
+
+
+class AuthTest(TestCase):
+    url = '/login/'
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        User.objects.create_user(username='username', password='password')
+
+    def get_client_and_data(self):
+        response = self.client.get(self.url)
+        self.client = response.client
+        data = {
+            'csrftoken': response.cookies['csrftoken'].value,
+            'username': 'username',
+            'password': 'password',
+        }
+        return data
+
+    def test_positive(self):
+        data = self.get_client_and_data()
+        response = self.client.post(self.url, data, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_invalid_password(self):
+        data = self.get_client_and_data()
+        data['password'] = 'invalid_password'
+        response = self.client.post(self.url, data, follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
+
+    def test_invalid_username(self):
+        data = self.get_client_and_data()
+        data['username'] = 'invalid_username'
+        response = self.client.post(self.url, data, follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
 
 
 class RegisterTest(TestCase):
