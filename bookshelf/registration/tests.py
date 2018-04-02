230c5bec9ab0dd0b4761a82e963a95dce70aebd6
rustan <rustanrr@mail.ru>
2018-04-02 15:42:09 +0400

    Add auth tests

--- bookshelf/registration/tests.py
+++ bookshelf/registration/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class AuthTest(TestCase):
+    url = '/login/'
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        User.objects.create_user(username='username', password='password')
+
+    def get_client_and_data(self):
+        response = self.client.get(self.url)
+        self.client = response.client
+        data = {
+            'csrftoken': response.cookies['csrftoken'].value,
+            'username': 'username',
+            'password': 'password',
+        }
+        return data
+
+    def test_positive(self):
+        data = self.get_client_and_data()
+        response = self.client.post(self.url, data, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_invalid_password(self):
+        data = self.get_client_and_data()
+        data['password'] = 'invalid_password'
+        response = self.client.post(self.url, data, follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
+
+    def test_invalid_username(self):
+        data = self.get_client_and_data()
+        data['username'] = 'invalid_username'
+        response = self.client.post(self.url, data, follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
