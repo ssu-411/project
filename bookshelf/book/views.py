d4d94c35c4e12ec240f637065a16fe6f1a4cabdc
Rafikov Rustan <rustanrr@mail.ru>
2018-04-16 14:31:56 +0400

    Add search (#72)

--- bookshelf/book/views.py
+++ bookshelf/book/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.template import Library
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import TemplateView
@@ -20,16 +21,25 @@ def prev_page(value):
 class BooksView(TemplateView):
     template_name = "books.html"
 
-    def get_context_data(self, page=0, **kwargs):
+    def get_context_data(self, page=0, *args, **kwargs):
+        query = self.request.GET.get('q', '').strip()
+        if query:
+            book_qs = Book.objects.filter(
+                Q(title__icontains=query) |
+                Q(author__name__icontains=query) |
+                Q(genre__name__icontains=query)
+            ).distinct()
+        else:
+            book_qs = Book.objects.all()
         context = super().get_context_data(**kwargs)
-        page = min(int(page), Book.objects.count() // BOOKS_ON_PAGE)
+        page = min(int(page), book_qs.count() // BOOKS_ON_PAGE)
         context['page'] = page
         context['prev_page'] = str(page - 1)
         start = page * BOOKS_ON_PAGE
-        books = Book.objects.all()[start:start + BOOKS_ON_PAGE]
+        books = book_qs[start:start + BOOKS_ON_PAGE]
 
         context['books'] = [books[x * BOOKS_IN_ROW: x * BOOKS_IN_ROW + BOOKS_IN_ROW] for x in range(ROWS_NUM)]
-        if len(Book.objects.all()) > start + BOOKS_ON_PAGE:
+        if book_qs.count() > start + BOOKS_ON_PAGE:
             context['next_page'] = page + 1
         return context
 
